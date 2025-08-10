c2d091d80af4368bee50a50c88cbe220426b2953
tw00n <shiningtwoon@gmail.com>
2025-08-10 23:18:58 +0900

    add try-catch for debugging on proxy.ts

--- src/app/proxy.ts
+++ src/app/proxy.ts
@@ -13,24 +13,28 @@ export async function request(formData: FormData) {
   const count = formData.getAll("count").map((e) => parseInt(e as string));
   const option = formData.get("option") as string;
 
-  let results!: Result[];
-  const requestTime = new Date().toISOString();
-  if(option == Option.local_GPU) {
-    results = await uploadImages("http://superres.invalab.com", images, count, localUploadImage);
-  } else if(option == Option.cloud_GPU) {
-    results = await uploadImages("http://10.0.2.8:81", images, count, cloudUploadImage);
-  } else if(option == Option.cloud_GPU_and_nestjs) {
+   try {
+     let results!: Result[];
+     const requestTime = new Date().toISOString();
+     if(option == Option.local_GPU) {
+       results = await uploadImages("http://superres.invalab.com", images, count, localUploadImage);
+     } else if(option == Option.cloud_GPU) {
+       results = await uploadImages("http://10.0.2.8:81", images, count, cloudUploadImage);
+     } else if(option == Option.cloud_GPU_and_nestjs) {
 
-  } else {
-    throw `${option}: 없는 옵션입니다.`;
-  }
-  const responseTime = new Date().toISOString();
+     } else {
+       throw `${option}: 없는 옵션입니다.`;
+     }
+     const responseTime = new Date().toISOString();
 
-  const sqlResult1 = await sql`INSERT INTO job(option, request, response) VALUES (${option}, ${requestTime}, ${responseTime}) RETURNING id`;
-  const jobId = sqlResult1.at(0)!.id;
-  results.forEach((result) => result.job_id = jobId);
+     const sqlResult1 = await sql`INSERT INTO job(option, request, response) VALUES (${option}, ${requestTime}, ${responseTime}) RETURNING id`;
+     const jobId = sqlResult1.at(0)!.id;
+     results.forEach((result) => result.job_id = jobId);
 
-  await sql`INSERT INTO test_data ${sql(results, "job_id", "task_id", "image_size", "success")}`;
+     await sql`INSERT INTO test_data ${sql(results, "job_id", "task_id", "image_size", "success")}`;
+   } catch(e) {
+     console.log(e);
+   }
 }
 
 async function uploadImages(host: string, images: File[], count: number[], uploadImage: (host: string, image: File) => Promise<Result>): Promise<Result[]> {
